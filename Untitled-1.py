c5553c33e4f54152a4b8930924af8b505fe6c81d
zhavei <zhavei.vei@gmail.com>
2023-10-24 15:46:17 +0700

    functions

--- Untitled-1.py
+++ Untitled-1.py
@@ -415,3 +415,16 @@ var_matt = np.array([[5, 0],
                     [1, -2]])
 result = var_matt * 2 
 print(result)
+
+#fungsi atau functions
+
+def mencari_luas_persegi_panjang (panjang, lebar):
+    luas_persegi_panjang = panjang * lebar
+    return luas_persegi_panjang
+
+persegi_panjang_pertama = mencari_luas_persegi_panjang(5,10)
+print(persegi_panjang_pertama)
+
+persegi_panjang_kedua = mencari_luas_persegi_panjang(4,15)
+print(persegi_panjang_kedua)
+
