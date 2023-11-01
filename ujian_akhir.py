06e58c8559ab95eec49e455abfe35c1c2062b49a
zhavei <zhavei.vei@gmail.com>
2023-11-01 16:22:35 +0700

    finish

---
+++ ujian_akhir.py
@@ -0,0 +1,20 @@
+# x = 'Dicoding'
+# x[0] = 'F'
+# print(x)
+
+print(dict([['name','Dicoding'],['age',17]]))
+
+x = { 'name': 'Coding', 'age': 20, 'isMarried': False }
+x ['name'] = "Dicoding"
+print(x)
+
+x = True 
+y = False
+print(x or y)
+
+lulus = True 
+print("Dicoding Indonesia") if lulus else print("Python")
+
+for i in range(1, 3):    
+    for j in range(1, 3):    
+        print(i,j) 
